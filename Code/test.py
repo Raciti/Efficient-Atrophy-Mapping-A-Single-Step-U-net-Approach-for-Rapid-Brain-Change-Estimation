24ff51164f6ebe80ef0abf814f0c3fec22e51898
Raciti <riccardo.raciti@yahoo.com>
2024-05-30 15:52:28 +0200

    fix code

--- Code/test.py
+++ Code/test.py
@@ -13,7 +13,7 @@ from utils import transform
 import time
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+
 
 basic_1 = Compose([
     LoadImageD(keys=["immA", "immB", "immGT"]),
@@ -39,11 +39,11 @@ if __name__ == '__main__':
 
 
     UNet = BasicUNet(spatial_dims=3, in_channels= 2, out_channels = 1, features=(32, 32, 64, 128, 256, 32)).to(device)
-    UNet.load_state_dict(torch.load("Model/Unet.pth"))
+    UNet.load_state_dict(torch.load("../Model/Unet.pth")) 
 
-    test_dataset = transform({"immA": "Data/A_halfwayto_B_brain.nii.gz", 
-                               "immB" : "Data/B_halfwayto_A_brain.nii.gz", 
-                               "immGT":"Data/A_to_B_flow.nii.gz"})
+    test_dataset = transform({"immA": "../Data/A_halfwayto_B_brain.nii.gz", 
+                               "immB" : "../Data/B_halfwayto_A_brain.nii.gz", 
+                               "immGT":"../Data/A_to_B_flow.nii.gz"})
 
     loss_function = losses.CustomMSELoss(2, "max")
 
@@ -61,17 +61,17 @@ if __name__ == '__main__':
     import nibabel as nib
     #Save output
     mri = nib.nifti1.Nifti1Image(((out.cpu().detach().numpy() - 2000) / 1000), None) 
-    mri.to_filename('Results/target.nii.gz')
+    mri.to_filename('../Results/target.nii.gz')
 
     #Save input 0
     mri = nib.nifti1.Nifti1Image(test_dataset["images"][0].squeeze(0).cpu().detach().numpy(),None)  
-    mri.to_filename('Results/input0.nii.gz')
+    mri.to_filename('../Results/input0.nii.gz')
     
     #Save input 1 
     mri = nib.nifti1.Nifti1Image(test_dataset["images"][1].squeeze(0).cpu().detach().numpy(),None)  
-    mri.to_filename('Results/input1.nii.gz')
+    mri.to_filename('../Results/input1.nii.gz')
 
     #Save GT
     mri = nib.nifti1.Nifti1Image(((test_dataset["immGT"].squeeze(0).cpu().detach().numpy() - 2000) / 1000),None)  
-    mri.to_filename('Results/GT.nii.gz')
+    mri.to_filename('../Results/GT.nii.gz')
     print(f"GT max: {mri.get_fdata().max()} \nGT min: {mri.get_fdata().min()}\n")
